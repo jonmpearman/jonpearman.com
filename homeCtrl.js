9559fcfe13e084fc28c64ee1a1260acd4a2c72cd
jonmpearman <celticjmp@gmail.com>
2016-08-04 21:57:27 -0700

    Add files via upload
    Updated Home page

---
+++ homeCtrl.js
@@ -0,0 +1,40 @@
+app.controller('homeCtrl', function() {
+	
+	var self = this;
+	
+	self.bio = {
+		
+		aboutWeb: 'My name is Jon Pearman and I&#39m an aspiring web developer. ' + 
+			'I have been developing for about a year and I&#39m completely self taught. ' + 
+			'Web development has become a very large passion in my life. I have always been &#39computer savy&#39 ' +
+			'but have fallen in love with development, specifically web development! ' +
+			'I enjoy learning new technologies, tackling different challenges, and growing my knowledge of the web altogether. ' +
+			'It&#39s something most people use every single day and it is only becoming a larger part of our lives! ' +
+			'Learning the ins and outs of this vast area has been a very enriching experience that I intend to continue ' +
+			'for the rest of my life. For me, web development has become a life style. It&#39s different from other ' +
+			'professions because it&#39s constantly improving, updating, innovating; that&#39s exciting! ' +
+			'It&#39s a life style because you can never stop learning. You can never stop improving. ' +
+			'There&#39s something very gratify about revisiting some code you wrote a month ago and thinking ' +
+			'of new ways to improve it, or to make it more maintainable, or just flat out change it completely.',
+		
+		aboutSoocer: 'Aside from web development I am hugely passionate about soccer. I have been a soccer play all my life ' +
+			'and always enjoy watching a game! I played D2 college soccer at Seattle Pacific University. ' +
+			'While at SPU I was voted team captain by my teammates, won multiple GNAC Defender of the Year, ' +
+			'multiple 1st team All-Conference (GNAC), multiple 1st team All-Region, and 2 All-American selections. ' +
+			'Soccer has been the single largest influence in my life. It&#39s taught me so many lessons ' +
+			'in leadership, working hard, being a team player, perseverance (never give up!), and I could go on and on. ' +
+			'I apply these lessons and mentalities to everything I do now. I never thought as I was going through all the 2 a day ' + 
+			'practices, or countless hours driving to and from practices/games, would soccer apply to anywhere but soccer! ' +
+			'I am thankful for all the experiences I gained through soccer and lessons it taught me along the way. ',
+		
+		aboutFood: 'Last but not least, I love food! Mainly eating and cooking it, but it isn&#39t too bad to ' +
+			'look at either! I have been trying to explore cooking more and more, getting inspiration from all the ' +
+			'cooking shows out there. My favorite food has to be pork chops! Which can be one of the trickiest proteins ' +
+			'to cook properly. I love to use new ingredients and try a recipe I haven&#39t done yet. ' +
+			'I also like to get creative on a budget, trying to elevate everyday foods into something truely elegant. ' +
+			'Taking items such as noodles and beef chunks and turning them into a delicious stir fry that can be ' +
+			'a completely new dish with different sauces! The simplicity yet creative tastes makes it worth the effort!',
+		
+		imgSrc: '',
+	};
+});
