825ff71c80699ee8d7a52f9ecb883000757209c7
jonmpearman <celticjmp@gmail.com>
2019-04-14 20:28:10 -0700

    Adding a 404 route and some error handling

--- server/server.js
+++ server/server.js
@@ -8,8 +8,33 @@ const PORT = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../react/dist`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/../react/dist/template.html`));
+const requestLogger = function(req, res, next) {
+  console.log(`Requesting ..... ${req.hostname}${req.originalUrl}`);
+  next();
+};
+
+app.use(requestLogger);
+
+app.get('/', [(req, res, next) => {
+  res.sendFile(path.resolve(`${__dirname}/../react/dist/template.html`), [], (err) => {
+    // throw error if file isn't found
+    if (err) next(err);
+    res.status(200);
+    console.log(`Request worked .... serving file ${path.resolve(`${__dirname}/../react/dist/template.html`)}`);
+  });
+}]);
+
+app.get('*', (req, res, next) => {
+  res.status(404);
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Page Not Found.' });
+    return;
+  }
+
+  // default to plain-text
+  res.type('txt').send('Page Not Found.');
 });
 
 app.listen(PORT, () => {
